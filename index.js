7b6c3b5097908aa658e3997b71e63b31601b1548
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-01-07 23:31:25 +0530

    Add Skelenton routes for user

--- index.js
+++ index.js
@@ -4,14 +4,45 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
 dotenv.config();
+
 const JWT_SECRET = process.env.JWT_SECRET;
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+app.post("/user/signup", (req,res)=>{
+    res.json({
+        message: "signup endpoint",
+    });
+});
+
+app.post("/user/signin", (req,res)=>{
+    res.json({
+        message: "signin endpoint",
+    });
+});
+
+app.post("/course/purchase", (req,res)=>{
+    res.json({
+        message: "time to purchase",
+    });
+});
+
+app.get("/user/purchases", (req,res)=>{
+    res.json({
+        message: "Purchased Courses",
+    });
+});
+
+app.get("/courses", (req,res)=>{
+    res.json({
+        message: "All courses endpoint",
+    });
+});
 
 
 app.listen(port, ()=>{
     console.log(`Server is Running on Port : ${port}`);
-})
+});
+
